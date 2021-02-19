874d61833a1cb24bc16a2abda1ed18f034758483
LuigiAttorresi <luigi.attorresi@mail.polimi.it>
2021-02-19 16:55:54 +0100

    Prova

--- prova.py
+++ prova.py
@@ -18,6 +18,8 @@ import crepe
 import ddsp
 import ddsp.training
 
+
+
 ''' PER PYTHONANYWHERE
 !sudo apt-get install libportaudio2 #In pythonanywhere potrebbe non servire (bug sounddevice)
 !pip install sounddevice
@@ -54,8 +56,6 @@ from scipy.io.wavfile import write
 #import IPython.display as ipd     #Togliere?
 from pathlib import Path
 from random import randrange
-
-print("fine")
 
 
 #%%
@@ -176,7 +176,7 @@ def record(seconds=3, sample_rate=DEFAULT_SAMPLE_RATE, normalize_db=0.1):
   """
   print('Starting recording for {} seconds...'.format(seconds))
   display.display(display.Javascript(record_js_code))
-  audio_string = output.eval_js('record(%d)' % (seconds * 1000.0))
+  #audio_string = output.eval_js('record(%d)' % (seconds * 1000.0)) NON FUNZIONA FUORI COLAB
   print('Finished recording!')
   audio_bytes = base64.b64decode(audio_string.split(',')[1])
   return audio_bytes_to_np(audio_bytes,
@@ -584,5 +584,373 @@ def auto_tune(f0_midi, tuning_factor, mask_on, amount=0.0, chromatic=False):
 
 
 
+#%%
+'''
+#Record or Upload Audio 
 
+record_or_upload = "Record"  #@param ["Upload (.mp3 or .wav)","Record"]
+
+record_seconds =     12#@param {type:"number", min:1, max:25, step:1}
+
+if record_or_upload == "Record":
+  audio = record(seconds=record_seconds)
+else:
+  # Load audio sample here (.mp3 or .wav3 file)
+  # Just use the first file.
+  filenames, audios = upload()
+  audio = audios[0][-1]
+audio = audio[np.newaxis, :]
+'''
 # %%
+'''
+import pyaudio
+import wave
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 2
+fs = 16000  # Record at 16000 samples per second
+seconds = 3
+filename = "output.wav"
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+print('Recording')
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+frames = []  # Initialize array to store frames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
+# Save the recorded data as a WAV file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
+'''
+# %%
+import sounddevice as sd
+# %%
+#Source Separation & Audio Features Extraction
+from spleeter.separator import Separator
+
+
+if __name__ == "__main__":
+  # Using embedded configuration.
+  separator = Separator('spleeter:4stems-16kHz')
+
+  separator.separate_to_file('Brani Prova\celeste.wav', 'Stems')
+
+  #write("Brani Prova\Fly Me - Cut.wav", 16000, audio.T)
+
+  vocals, sr = librosa.load('Stems\celeste/vocals.wav', sr=16000)
+  other, sr = librosa.load('Stems\celeste/other.wav', sr=16000)
+  drums, sr = librosa.load('Stems\celeste/drums.wav', sr=16000)
+  bass, sr = librosa.load('Stems\celeste/bass.wav', sr=16000)
+
+  #Serve solo per i modelli armonici
+  hop_length = 256
+  frame_length = 512
+  vocals_rms = librosa.feature.rms(vocals, frame_length=frame_length, hop_length=hop_length, center=True)
+  drums_rms = librosa.feature.rms(drums, frame_length=frame_length, hop_length=hop_length, center=True)
+  bass_rms = librosa.feature.rms(bass, frame_length=frame_length, hop_length=hop_length, center=True)
+
+  THRESHOLD = 0.0001
+
+  if np.mean(vocals_rms) > THRESHOLD:
+    vocals = vocals[np.newaxis, :]
+    print("The track contains vocals")
+  if np.mean(bass_rms) > THRESHOLD:
+    print("The track contains bass")
+    bass = bass[np.newaxis, :]
+  if np.mean(drums_rms) > THRESHOLD:
+    print("The track contains drums")
+  if (np.mean(vocals_rms) <= THRESHOLD) & (np.mean(bass_rms) <= THRESHOLD) & (np.mean(drums_rms) <= THRESHOLD):
+    print("The track needs to have at least a singing voice or a bass line or a drum pattern!")
+
+
+  # Setup the session.
+  ddsp.spectral_ops.reset_crepe()
+
+  # Compute features.
+  audio_features = ddsp.training.metrics.compute_audio_features(vocals)
+  audio_features['loudness_db'] = audio_features['loudness_db'].astype(np.float32)
+  audio_features_mod = None
+
+
+
+  TRIM = -15
+  
+  # %%
+  #Choose the sound for vocals
+
+  ''' DA FARE
+  - Dare la possibilità all'utente di scegliere il soundscape. Sulla base di quello e 
+  delle energie dei segnali ad ogni traccia viene assegnato un modello e rispettivi
+  parametri di loudness e pitch shift
+
+  - Gestire i file dei modelli
+  '''
+
+  #model = 'BarcaTD 30K' #@param ['Anatra 10K', 'Anatra 20K', 'Anatra 30K Sbagliata', 'Anatra 30K', 'Anatra 40K', 'Motosega 10K', 'Lupo 30K', 'Barca 22K', 'Barca 30K', 'Barca 40K', 'Locomotiva 22K', 'Locomotiva 30K', 'Locomotiva 40K', 'Foca 20K', 'Foca 30K', 'Foca 40K', 'Rana 20K', 'Rana 30K', 'Mosca 8K', 'Mosca 12K', 'Mosca 20K', 'Mosca 30K', 'Gabbiano 20K', 'Gabbiano 30K', 'Gabbiano 40K', 'Treno 20K', 'Treno 30K', 'Treno 40K', 'Mucca 24K', 'Mucca 30K', 'Mucca 40K', 'BarcaTD 16K', 'BarcaTD 20K', 'BarcaTD 30K', 'BarcaTD 40K']
+  #MODEL = model
+
+  # Harmonic part
+
+  def find_model_dir(dir_name):
+    # Iterate through directories until model directory is found
+    for root, dirs, filenames in os.walk(dir_name):
+      for filename in filenames:
+        if filename.endswith(".gin") and not filename.startswith("."):
+          model_dir = root
+          break
+    return model_dir 
+
+  ''' PER DOPO
+  if model == 'Anatra 10K':
+    #FOLEY_PATH = '/content/drive/MyDrive/Prototipo_CPAC/Forest/Forest_foley/'
+    model_dir = find_model_dir('/content/drive/MyDrive/Soundscapes014/Anatra10K')
+    #BACKGROUND_PATH = '/content/drive/MyDrive/Prototipo_CPAC/Forest/Background/snow_forest.wav'
+    #mix = 0.9
+    #pitch_shift = 0
+  '''
+  model_dir = find_model_dir('Models\Anatra014 10K')
+  gin_file = os.path.join(model_dir, 'operative_config-0.gin')
+
+  '''
+  # Load the dataset statistics.
+  DATASET_STATS = None
+  dataset_stats_file = os.path.join(model_dir, 'dataset_statistics.pkl')
+  print(f'Loading dataset statistics from {dataset_stats_file}')
+
+  
+  try:
+    if tf.io.gfile.exists(dataset_stats_file):
+      with tf.io.gfile.GFile(dataset_stats_file, 'rb') as f:
+        DATASET_STATS = pickle.load(f)
+  
+  except Exception as err:
+    print('Loading dataset statistics from pickle failed: {}.'.format(err))
+  '''
+
+  # Parse gin config,
+  with gin.unlock_config():
+    gin.parse_config_file(gin_file, skip_unknown=True)
+
+  # Assumes only one checkpoint in the folder, 'ckpt-[iter]`.
+  ckpt_files = [f for f in tf.io.gfile.listdir(model_dir) if 'ckpt' in f]
+  ckpt_name = ckpt_files[0].split('.')[0]
+  ckpt = os.path.join(model_dir, ckpt_name)
+
+  # Ensure dimensions and sampling rates are equal
+  time_steps_train = gin.query_parameter('DefaultPreprocessor.time_steps')
+  n_samples_train = gin.query_parameter('Additive.n_samples')
+  hop_size = int(n_samples_train / time_steps_train)
+
+  time_steps = int(vocals.shape[1] / hop_size)
+  n_samples = time_steps * hop_size
+
+
+  gin_params = [
+      'Additive.n_samples = {}'.format(n_samples),
+      'FilteredNoise.n_samples = {}'.format(n_samples),
+      'DefaultPreprocessor.time_steps = {}'.format(time_steps),
+      'oscillator_bank.use_angular_cumsum = True',  # Avoids cumsum accumulation errors.
+  ]
+
+  with gin.unlock_config():
+    gin.parse_config(gin_params)
+
+
+  # Trim all input vectors to correct lengths 
+  for key in ['f0_hz', 'f0_confidence', 'loudness_db']:
+    audio_features[key] = audio_features[key][:time_steps]
+  #audio_features['audio'] = audio_features['audio'][:, :n_samples]
+
+
+  # Set up the model just to predict audio given new conditioning
+  model = ddsp.training.models.Autoencoder()
+  model.restore(ckpt)
+
+  # Build model by running a batch through it.
+  _ = model(audio_features, training=False)
+
+
+  # %% 
+
+  # Resynthesize Vocals
+
+
+  threshold = 1 
+
+  ADJUST = True
+
+  quiet = 20 
+
+  autotune = 0 
+
+  loudness_shift = 0 
+
+  pitch_shift = 0
+
+
+  audio_features_mod = {k: v.copy() for k, v in audio_features.items()}
+
+
+  ## Helper functions.
+  def shift_ld(audio_features, ld_shift=0.0):
+    """Shift loudness by a number of ocatves."""
+    audio_features['loudness_db'] += ld_shift
+    return audio_features
+
+
+  def shift_f0(audio_features, pitch_shift=0.0):
+    """Shift f0 by a number of ocatves."""
+    audio_features['f0_hz'] *= 2.0 ** (pitch_shift)
+    audio_features['f0_hz'] = np.clip(audio_features['f0_hz'], 
+                                      0.0, 
+                                      librosa.midi_to_hz(110.0))
+    return audio_features
+
+
+  mask_on = None
+
+
+  ''' PRIMA BISOGNA RISOLVERE PICKLE
+  if ADJUST and DATASET_STATS is not None:
+    # Detect sections that are "on".
+    mask_on, note_on_value = detect_notes(audio_features['loudness_db'],
+                                          audio_features['f0_confidence'],
+                                          threshold)
+
+    if np.any(mask_on):
+      # Shift the pitch register.
+      target_mean_pitch = DATASET_STATS['mean_pitch']
+      pitch = ddsp.core.hz_to_midi(audio_features['f0_hz'])
+      mean_pitch = np.mean(pitch[mask_on])
+      p_diff = target_mean_pitch - mean_pitch
+      p_diff_octave = p_diff / 12.0
+      round_fn = np.floor if p_diff_octave > 1.5 else np.ceil
+      p_diff_octave = round_fn(p_diff_octave)
+      audio_features_mod = shift_f0(audio_features_mod, p_diff_octave)
+
+
+      # Quantile shift the note_on parts.
+      _, loudness_norm = fit_quantile_transform(
+          audio_features['loudness_db'],
+          mask_on,
+          inv_quantile=DATASET_STATS['quantile_transform'])
+
+      # Turn down the note_off parts.
+      mask_off = np.logical_not(mask_on)
+      loudness_norm[mask_off] -=  quiet * (1.0 - note_on_value[mask_off][:, np.newaxis])
+      loudness_norm = np.reshape(loudness_norm, audio_features['loudness_db'].shape)
+      
+      audio_features_mod['loudness_db'] = loudness_norm 
+
+
+      # Auto-tune.
+      #if autotune:
+        #f0_midi = np.array(ddsp.core.hz_to_midi(audio_features_mod['f0_hz']))
+        #tuning_factor = get_tuning_factor(f0_midi, audio_features_mod['f0_confidence'], mask_on)
+        #f0_midi_at = auto_tune(f0_midi, tuning_factor, mask_on, amount=autotune)
+        #audio_features_mod['f0_hz'] = ddsp.core.midi_to_hz(f0_midi_at)
+
+
+    else:
+      print('\nSkipping auto-adjust (no notes detected or ADJUST box empty).')
+
+  else:
+    print('\nSkipping auto-adujst (box not checked or no dataset statistics found).')
+  '''
+
+
+  # Manual Shifts.
+  audio_features_mod = shift_ld(audio_features_mod, loudness_shift)
+  audio_features_mod = shift_f0(audio_features_mod, pitch_shift)
+
+  af = audio_features if audio_features_mod is None else audio_features_mod
+
+  # Run a batch of predictions.
+  outputs = model(af, training=False)
+  audio_gen = model.get_audio_from_outputs(outputs)
+
+  ''' Per il background
+  if len(background) >= audio_gen.shape[-1]:
+    background = background[0:audio_gen.shape[-1]]
+
+  '''
+
+  print("0")
+
+  import scipy.io.wavfile as wave
+  wave.write('Generated_vocals.wav', 16000, audio_gen)
+  # QUI SUCCEDE QUALCOSA; NON VA OLTRE
+  print("1")
+
+  # %% 
+  # Omnizart Transcribe and Resynthesize
+  if np.mean(drums_rms) > THRESHOLD:
+
+    uploaded_audio = 'Stems\celeste/drums.wav'
+
+    mode = "drum" 
+
+    from omnizart.drum import app as dapp
+
+    midi = dapp.transcribe(f"{uploaded_audio}", model_path=None)
+
+    #import scipy.io.wavfile as wave
+    from omnizart.remote import download_large_file_from_google_drive
+
+    print("2")
+
+    '''
+    SF2_FILE = "forest_temp.sf2"
+    if not os.path.exists(SF2_FILE):
+      print("Downloading soundfont...")
+      download_large_file_from_google_drive(
+          "1gMpSiDI46HtAe_RIyFx6dFD3d6KC2TDn",
+          save_name=SF2_FILE
+        )
+    '''
+
+    SF2_FILE = 'soundfont_prova.sf2'
+
+    out_name = f"{uploaded_audio}_synth.wav"
+    raw_wav = midi.fluidsynth(fs=44100, sf2_path=SF2_FILE)
+    print("3")
+
+    wave.write(out_name, 44100, raw_wav)
+    print("4")
+    soundscape_drum, sr = librosa.load(out_name, sr=16000)
+
+  else:
+    print('No drums found!')
+
+  # %%
+  # Final Mix
+
+  mix = audio_gen + soundscape_drum[0:np.shape(audio_gen)[1]]
+  wave.write('Final_mix', 44100, mix)
+
+  print("Done!")
+
