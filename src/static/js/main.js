948b1de6938e727d1efab972afe6502a258f9b1b
LuigiAttorresi <luigi.attorresi@mail.polimi.it>
2021-02-26 22:42:02 +0100

    GUI Update

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,8 +1,13 @@
-var element = document.getElementById('mySwipe'),
-    prevBtn = document.getElementById('prev'),
-    nextBtn = document.getElementById('next');
+var upload_swipe = document.getElementById('uploadSwipe'),
+    soundscape_swipe = document.getElementById('sdounscapeSwipe'),
+    prevBtnUpload = document.getElementById('rightArrowUpload'),
+    nextBtnUpload = document.getElementById('leftArrowUpload'),
+    prevBtnSoundscape = document.getElementById('rightArrowSoundscape'),
+    nextBtnSoundscape = document.getElementById('rightArrowSoundscape'),
+    mic = document.getElementById('microphone');
 
-window.mySwipe = new Swipe(element, {
+
+soundscape_swipe = new Swipe(soundscape_swipe, {
   startSlide: 0,
   auto: 0,
   draggable: false,
@@ -10,10 +15,30 @@ window.mySwipe = new Swipe(element, {
   continuous: true,
   disableScroll: true,
   stopPropagation: true,
-  callback: function(index, element) {},
-  transitionEnd: function(index, element) {}
+  callback: function(index, soundscape_swipe) {},
+  transitionEnd: function(index, soundscape_swipe) {}
+});
+
+upload_swipe = new Swipe(upload_swipe, {
+  startSlide: 0,
+  auto: 0,
+  draggable: false,
+  autoRestart: false,
+  continuous: true,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, upload_swipe) {},
+  transitionEnd: function(index, upload_swipe) {}
 });
 
 
-prevBtn.onclick = mySwipe.prev;
-nextBtn.onclick = mySwipe.next;
+
+prevBtnUpload.onclick = upload_swipe.prev;
+nextBtnUpload.onclick = upload_swipe.next;
+
+prevBtnSoundscape.onclick = soundscape_swipe.prev;
+nextBtnSoundscape.onclick = soundscape_swipe.next;
+mic.onclick = mic.classList.toggle('active')
+//mic.onclick = console.log("click")
+
+
