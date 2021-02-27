5e663091dbd7966c8bfa8789edef1de8b8f05867
LuigiAttorresi <luigi.attorresi@mail.polimi.it>
2021-02-27 10:02:56 +0100

    Update GUI

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,60 @@
+var elementSoundscape = document.getElementById('mySwipeSoundscape'),
+    elementModality = document.getElementById('mySwipeModality'),
+    prevBtnSoundscape = document.getElementById('prevSoundscape'),
+    nextBtnSoundscape = document.getElementById('nextSoundscape'),
+    prevBtnModality = document.getElementById('prevModality'),
+    nextBtnModality = document.getElementById('nextModality');
+
+window.mySwipeSoundscape = new Swipe(elementSoundscape, {
+  startSlide: 0,
+  auto: 0,
+  draggable: false,
+  autoRestart: false,
+  continuous: true,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, elementSoundscape) {},
+  transitionEnd: function(index, elementSoundscape) {}
+});
+
+window.mySwipeModality = new Swipe(elementModality, {
+  startSlide: 0,
+  auto: 0,
+  draggable: false,
+  autoRestart: false,
+  continuous: true,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, elementModality) {},
+  transitionEnd: function(index, elementModality) {}
+});
+
+let index = 0;
+
+let mod = function (x, m) {
+    return (x%m + m)%m;
+}
+
+let prevSelectedSoundscape = function () {
+    mySwipeSoundscape.prev();
+    document.getElementById("soundscape_selection").value = scapes[mod(--index, scapes.length)];
+    console.log(document.getElementById("soundscape_selection").value)
+}
+
+let nextSelectedSoundscape = function () {
+    mySwipeSoundscape.next();
+    document.getElementById("soundscape_selection").value = scapes[mod(++index, scapes.length)];
+    console.log(document.getElementById("soundscape_selection").value)
+}
+
+
+prevBtnSoundscape.onclick = prevSelectedSoundscape;
+nextBtnSoundscape.onclick = nextSelectedSoundscape;
+
+prevBtnModality.onclick = mySwipeModality.prev;
+nextBtnModality.onclick = mySwipeModality.next;
+
+/*
 var upload_swipe = document.getElementById('uploadSwipe'),
     soundscape_swipe = document.getElementById('sdounscapeSwipe'),
     prevBtnUpload = document.getElementById('rightArrowUpload'),
@@ -41,4 +98,4 @@ nextBtnSoundscape.onclick = soundscape_swipe.next;
 mic.onclick = mic.classList.toggle('active')
 //mic.onclick = console.log("click")
 
-
+*/
